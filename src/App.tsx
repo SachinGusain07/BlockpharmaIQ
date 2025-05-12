fc92ed1cf1f113eb97dd63fcaa5f28b017ab1064
Ashish bhatt <117436056+Ashish0197@users.noreply.github.com>
2025-05-12 12:35:35 +0530

    supplier dashboard working

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,41 @@ import BasicLayout from '@/layout/BasicLayout'
 import Home from '@/pages/home'
 import Login from '@/pages/login'
 import Signup from '@/pages/signup'
-// import { lazy } from 'react'
-import SuperAdminDashboard from '@/components/SuperAdmin/Dashboard'
-import SupplierDashboard from '@/components/Supplier/Dashboard'
+import { lazy } from 'react'
+// import SuperAdminDashboard from '@/components/SuperAdmin/Dashboard'
+// import SupplierDashboard from '@/components/Supplier/Dashboard'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import AccessDenied from './components/AccessDenied'
 import { PharmacyOrderSystem } from './components/Pharmacy/Order'
-import Pharmacies from './components/SuperAdmin/Pharmacy'
-import Suppliers from './components/SuperAdmin/Supplier'
-import Users from './components/SuperAdmin/User'
+// import Pharmacies from './components/SuperAdmin/Pharmacy'
+// import Suppliers from './components/SuperAdmin/Supplier'
+// import Users from './components/SuperAdmin/User'
 import VendorProductsPage from './components/Supplier/ProductPage'
 import SupplierSettings from './components/Supplier/Setting'
-import SupplierOrdersPage from './components/Supplier/SupplierOrders'
-import AccountSettings from './components/UserSettings/AccountSetting'
+// import SupplierOrdersPage from './components/Supplier/SupplierOrders'
+// import AccountSettings from './components/UserSettings/AccountSetting'
 import DashboardLayout from './layout/DashboardLayout'
 import ProtectedRoute from './layout/ProtectedRoute'
-import ChangePassword from './pages/change-password'
-import InventoryPage from './pages/inventory'
-import NotFound from './pages/not-found'
-import PharmacySetting from './pages/pharmacy-setting'
-import ProfileInformation from './pages/profile-info'
-import UpdateProfile from './pages/update-profile'
+// import ChangePassword from './pages/change-password'
+// import InventoryPage from './pages/inventory'
+// import NotFound from './pages/not-found'
+// import PharmacySetting from './pages/pharmacy-setting'
+// import ProfileInformation from './pages/profile-info'
+// import UpdateProfile from './pages/update-profile'
 
-// const ProfileInformation = lazy(() => import('@/pages/profile-info'))
-// const AccountSettings = lazy(() => import('@/pages/account-setting'))
-// const ChangePassword = lazy(() => import('@/pages/change-password'))
-// const UpdateProfile = lazy(() => import('@/pages/update-profile'))
-// const NotFound = lazy(() => import('@/pages/not-found'))
-// const InventoryPage = lazy(() => import('@/pages/inventory'))
-// const OrdersPage = lazy(() => import('@/pages/order'))
-// const PharmacySetting = lazy(() => import('@/pages/pharmacy-setting'))
-// const SuperAdminDashboard = lazy(() => import('@/components/SuperAdmin/Dashboard'))
-// const Pharmacies = lazy(() => import('@/components/SuperAdmin/Pharmacy'))
-// const Suppliers = lazy(() => import('@/components/SuperAdmin/Supplier'))
-// const Users = lazy(() => import('@/components/SuperAdmin/User'))
-// const SupplierDashboard = lazy(() => import('@/components/Supplier/Dashboard'))
-// const OrderHistoryPage = lazy(() => import('@/components/Supplier/OrderHistory'))
-// const SupplierOrdersPage = lazy(() => import('@/components/Supplier/SupplierOrders'))
+const ProfileInformation = lazy(() => import('@/pages/profile-info'))
+const AccountSettings = lazy(() => import('@/pages/account-setting'))
+const ChangePassword = lazy(() => import('@/pages/change-password'))
+const UpdateProfile = lazy(() => import('@/pages/update-profile'))
+const NotFound = lazy(() => import('@/pages/not-found'))
+const InventoryPage = lazy(() => import('@/pages/inventory'))
+const PharmacySetting = lazy(() => import('@/pages/pharmacy-setting'))
+const SuperAdminDashboard = lazy(() => import('@/components/SuperAdmin/Dashboard'))
+const Pharmacies = lazy(() => import('@/components/SuperAdmin/Pharmacy'))
+const Suppliers = lazy(() => import('@/components/SuperAdmin/Supplier'))
+const Users = lazy(() => import('@/components/SuperAdmin/User'))
+const SupplierDashboard = lazy(() => import('@/components/Supplier/Dashboard'))
+const SupplierOrdersPage = lazy(() => import('@/components/Supplier/SupplierOrders'))
 
 export const App = () => {
   return (
